7d336dd89a8c215677b9f4fc3c892d7ba82be7f6
Ashwani Senapati <senapatiashwani47@gmail.com>
2026-06-06 17:02:11 +0530

    chore:spons page

---
+++ app/callforspons/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import OtherBackground from '../../components/OtherBackground'
+
+function page() {
+  return (
+    <div>
+      <OtherBackground/>
+      <div className='py-12'>
+      {/*code here*/}
+      </div>
+    </div>
+  )
+}
+
+export default page
